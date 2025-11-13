0bfdbf3692980b7d0408214d14a2e60084ed1d4b
Jackal <mofizusmani1@gmail.com>
2025-11-13 17:03:20 +0530

    Day5

---
+++ Day5.py
@@ -0,0 +1,16 @@
+print(3+5) # Output: 8
+print(2.5 * 4) # Output: 10.0
+print(10 - 2) # Output: 8
+
+# Division (Float Division) Always gives a decimal (float) result
+print(9 / 3) # Output: 3.0
+
+
+# Floor Division (Integer Division) 
+# Gives the whole number part only, removing everything after the decimal.
+# It basically rounds down to the nearest integer.
+print(9 // 3) # Output: 3
+
+
+# In Python, ** means exponentiation — or simply, “raise to the power of.”
+print(5 ** 3)
