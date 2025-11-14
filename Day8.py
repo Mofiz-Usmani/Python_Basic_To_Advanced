ff36f66d9e6b1d99f763b2b4259e7192ec8c9f20
Jackal <mofizusmani1@gmail.com>
2025-11-14 10:54:52 +0530

    Day8

--- Day8.py
+++ Day8.py
@@ -1,47 +1,89 @@
-#==================
-# Input Functions
-#==================
+# #==================
+# # Input Functions
+# #==================
 
 
 
-# a = input("Enter something: ")
-# print(a)
-# print(type(a))   #Output: <class 'str'> By default, input() returns a string.
+# # a = input("Enter something: ")
+# # print(a)
+# # print(type(a))   #Output: <class 'str'> By default, input() returns a string.
 
 
-# b = int(input("Enter an integer: "))   # Convert input string to integer
-# c = int(input("Enter another integer: "))  # Convert input string to integer
-# b = b + c
-# print(b)   #Output: Sum of two integers
-
-
-
-
-
-# d = float(input("Enter a float number: "))  # Convert input string to float
-# e = float(input("Enter another float number: "))  # Convert input string to float
-
-
-
-A = input("Enter first number: ")
-B = input("Enter second number: ")
-print(int(A) + int(B))  # Convert inputs to integers and add
-print(float(A) + float(B))  # Convert inputs to floats and add
+# # b = int(input("Enter an integer: "))   # Convert input string to integer
+# # c = int(input("Enter another integer: "))  # Convert input string to integer
+# # b = b + c
+# # print(b)   #Output: Sum of two integers
 
 
 
 
 
-# Take Multiple Input in Python
-
-x, y = input("Enter two values: ").split()
-print("Number of boys: ", x)
-print("Number of girls: ", y)
+# # d = float(input("Enter a float number: "))  # Convert input string to float
+# # e = float(input("Enter another float number: "))  # Convert input string to float
 
 
 
+# A = input("Enter first number: ")
+# B = input("Enter second number: ")
+# print(int(A) + int(B))  # Convert inputs to integers and add
+# print(float(A) + float(B))  # Convert inputs to floats and add
 
-x, y, z = input("Enter three values: ").split()
-print("Total number of students: ", x)
-print("Number of boys is : ", y)
-print("Number of girls is : ", z)
+
+
+
+
+# # Take Multiple Input in Python
+
+# x, y = input("Enter two values: ").split()
+# print("Number of boys: ", x)
+# print("Number of girls: ", y)
+
+
+
+
+# x, y, z = input("Enter three values: ").split()
+# print("Total number of students: ", x)
+# print("Number of boys is : ", y)
+# print("Number of girls is : ", z)
+
+
+
+
+
+
+
+
+# Dynamic Typing : 
+
+# Python variables are dynamically typed, meaning the same variable can hold 
+# different types of values during execution.
+
+x = 10
+x = "Now a string"
+print(x)  #Output : Now a string
+
+
+
+
+
+
+# Multiple Assignments : 
+# Python allows multiple variables to be assigned values in a single line.
+
+# Assigning the Same Value
+# Python allows assigning the same value to multiple variables in a single line, 
+# which can be useful for initializing variables with the same value.
+
+a = b = c = 100
+print(a, b, c)    #Output : 100 100 100
+
+
+
+
+# Assigning Different Values
+# We can assign different values to multiple variables simultaneously, making the code concise and easier to read.
+
+x, y, z = 1, 2.5, "Python"
+print(x, y, z)
+
+
