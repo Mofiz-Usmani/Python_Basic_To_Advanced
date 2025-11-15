5e05434a9ad493d44ce072163ac18f47507e66f3
Jackal <mofizusmani1@gmail.com>
2025-11-15 12:28:43 +0530

    Day11

---
+++ Day11.py
@@ -0,0 +1,12 @@
+# ==================
+# For Loop in Python
+# ==================
+
+
+
+
+# Example 1: Loop over a list
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
