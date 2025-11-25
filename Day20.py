84bb0d876cc5cc9a3924698e942958480a34eb93
Jackal <mofizusmani1@gmail.com>
2025-11-25 16:09:22 +0530

    Day20

---
+++ Day20.py
@@ -0,0 +1,21 @@
+# ==============================
+# Exception Handling in Python
+# ==============================
+
+
+
+num = input("Enter the Number: \n")
+
+print(f"Multiplication Table of {num} is : \n")
+
+
+
+
+
+
+for i in range(1, 11):
+    print(f"{int(num)} X {i} = {int(num)*i}")
+
+
+
+print("End of Program!!!")
