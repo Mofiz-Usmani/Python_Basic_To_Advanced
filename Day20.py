f1bfb29fc461e34a69209d3777d9a8160b03fec3
Jackal <mofizusmani1@gmail.com>
2025-11-25 16:10:32 +0530

    Day20

--- Day20.py
+++ Day20.py
@@ -9,12 +9,11 @@ num = input("Enter the Number: \n")
 print(f"Multiplication Table of {num} is : \n")
 
 
-
-
-
-
-for i in range(1, 11):
-    print(f"{int(num)} X {i} = {int(num)*i}")
+try:
+    for i in range(1, 11):
+        print(f"{int(num)} X {i} = {int(num)*i}")
+except Exception as e: 
+    print(e)
 
 
 
