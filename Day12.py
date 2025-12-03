bc0074a459f2768068c67f48e9a11341064913b3
Jackal <mofizusmani1@gmail.com>
2025-12-03 10:57:13 +0530

    Day12

--- Day12.py
+++ Day12.py
@@ -129,8 +129,30 @@ rev = 0
 while n > 0:
     rev = rev * 10 + n % 10
     n //= 10
-    
+
 print("Reversed number:", rev)
+
+
+
+
+
+
+
+#? Check if a number is palindrome
+
+n = int(input("Enter a number: "))
+
+temp = n
+rev = 0
+
+while n > 0:
+    rev = rev * 10 + n % 10
+    n //= 10
+    
+if temp == rev:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
 
 
 
