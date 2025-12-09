2ade481763e0cba08c3494634858bc6d9855c807
Jackal <mofizusmani1@gmail.com>
2025-12-09 08:28:20 +0530

    shorthand if else

--- ShortHand_if_else.py
+++ ShortHand_if_else.py
@@ -2,3 +2,28 @@ a = 10
 b = 5
 
 if a > b: print("a is bigger")
+
+
+
+
+
+# One-Line IF-ELSE (Ternary)
+a = 3
+b = 7
+
+print("a wins") if a > b else print("b wins")
+
+
+
+
+
+
+
+
+
+
+# Assign Value Using Ternary
+age = 20
+
+status = "Adult" if age >= 18 else "Minor"
+print(status)
